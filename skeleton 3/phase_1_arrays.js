e78c0a618bdf65854cd00102d4eb541365812630
Evan-Leon <evan.leon.j@gmail.com>
2021-04-14 14:44:28 -0400

    Finished uniq and twosum

--- skeleton 3/phase_1_arrays.js
+++ skeleton 3/phase_1_arrays.js
@@ -1,12 +1,32 @@
 Array.prototype.uniq = function ()  {
-    let uniqArray =[]
+    let uniqArray =[];
 
-     for (let i= 0 , i < this.length, i++) {
+    for (let i= 0 ; i < this.length; i++) {
 
-         if (uniqArray.includes(this[i]))
-         uniqArray.push(i)
-     }    
+        if (!uniqArray.includes(this[i])){
+         uniqArray.push(this[i]);
+        }    
     }
     return uniqArray;
 }
+
+Array.prototype.twoSum = function () {
+    
+    let sumArray = [];
+    
+    for (let i= 0 ; i < this.length; i++) {
+        
+        for (let j = i +1; j < this.length; j++){
+            if (this[i] + this[j] == 0){
+                sumArray.push([i, j]);
+            }
+        }
+
+
+    }
+    console.log(`${sumArray}`);
+    return sumArray;
+}
+
+
 
